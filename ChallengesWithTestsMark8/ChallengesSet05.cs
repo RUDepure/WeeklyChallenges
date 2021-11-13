cad41be08bfb7ffe7ae36bfebe9786ffde6d1752
RubyBugFix <alexrum92@gmail.com>
2021-11-12 23:52:11 -0600

    weekly challenges 5

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -7,12 +7,20 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            return startNumber + (n - (startNumber % n));
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            for (int i = 0; i < businesses.Length; i++)
+            {
+                if (businesses[i].TotalRevenue == 0)
+                    businesses[i].Name = "CLOSED";
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
@@ -22,12 +30,32 @@ namespace ChallengesWithTestsMark8
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int sumNumbers = 0;
+
+            if (numbers == null)
+                return 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0 && i != numbers.Length-1)
+                {
+                    sumNumbers += numbers[i + 1];
+                }
+            }
+
+            return sumNumbers;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            string sentence = "";
+
+            foreach (var word in words)
+                sentence = sentence + " " + word;
+
+            return sentence;
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
